f3ebe5aeefaac095c3c7f835c283a0fda663607d
martact <martacatto@gmail.com>
2021-12-14 11:14:32 +0100

    added comments

--- kern/vm/coremap.c
+++ kern/vm/coremap.c
@@ -6,77 +6,46 @@
 #include "coremap.h"
 
 
+//andare a vedere in OS161 MEMORY slide --> pagina 19 
+
+
 
 /*si può utilizzare una struttura coremap_entry per tenere traccia delle informazioni sulla pagina fisica
-coremap sarà un array di coremap_entry
+coremap sarà un array di coremap_entry.
+Deve indicare se il frame è libero, se è occupato, indica quanti frame contigui per la stessa locazione
 
-coremap entry - where is mapped (virtual address --> for swapping) 
-              - page status ( free, clean , fixed ...) 
 
-*/
-/*
 coremap entry
 coremap ( tipo bitmap ) 
+
 */
 
-// /* Initialization function of the Virtual Memory System  */
-
-
-int coremap_init( size_t ram_size) {
-//     //allocata tra firstaddr e freeaddr
-//     //tra 0 e freeaddr index --> pagine fixed
-//     //tra freeaddr e lastaddr --> pagne free
+// /* Initialization function of the CoreMap  */
+int coremap_init( size_t ram_size, vaddr_t first_free_addr) {
+//     //allocata tra firstaddr(0) e freeaddr --> questa parte in realtà si può non mappare perchè non sono frame che si possono liberare 
+//     //tra 0 e freeaddr index --> pagine fixed ( di cui non si può fare swap out )  
+//     //tra freeaddr e lastaddr(ram_size) --> pagine free
  }
 
+//Deve trovare un segmento libero 
+void coremap_find_free (void){
+    //cercare un frame libero
+    //se libero chiama la funzione che lo occupi
+    //se non c'è nessun frame libero 
+    //chiama la funzione che faccia swap out di qualcosa
+}
 
-int vm_is_active(void){
-    //si può usare per verificare chela mmeoria virtuale sia stata bootstrappata
-    //si fa una variabile globale che viene settate alla fine di Vm_bootstrap 
-    if( 1 ) //se non è stata bootrappata
-        return 0;
-    else  // se è stata bootstrappata 
-        return 1 ; 
-
+//Deve trovare N segmenti liberi contigui
+void coremap_find_nfree(void/*int num_seg*/){
+    //cerca n frame contigui liberi 
+    //se li trova, chiama la funzione che li occupi
+    //se non li trova
+    //chiama la funzione che li trovi
 }
 
 
 
 
-/* Allocate kernel heap pages (called by kmalloc/kfree) */
-vaddr_t alloc_kpages(unsigned npages){
-    npages++; 
-    if ( vm_is_active() ){
-        //routine post vm_bootstrap
-        //prevede di utilizare page_nalloc
 
-    }
-    else {
-        //routine pre vm_bootstrap
-        //prevede di utilizzare getppages ( penso si possa recuperare il pezzo da dumbvm )
 
-    }
-    return 2 ; 
-}
 
-/* Free kernel heap pages (called by kmalloc/kfree) */
-void free_kpages(vaddr_t addr){
-     if ( vm_is_active() ){
-        //routine post vm_bootstrap
-        //prevede di utilizare page_free --> ricordiamo che si deve tenere conto di quante sono le pagine contigue allocate quando si allocano in page_nalloc
-
-    }
-    else {
-        //routine pre vm_bootstrap
-        //prevede di utilizzare freeppages( penso si possa recuperare il pezzo da dumbvm )
-
-    }
-    (int)addr++; 
-}
-
-/* TLB shootdown handling called from interprocessor_interrupt */
- void
-vm_tlbshootdown(const struct tlbshootdown *ts)
-{
-	(void)ts;
-	panic("dumbvm tried to do tlb shootdown?!\n");
-}
