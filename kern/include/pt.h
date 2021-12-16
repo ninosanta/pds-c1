870daa9f12fe1ae9c07064cd9e5667ef34b08d55
martact <martacatto@gmail.com>
2021-12-16 11:01:11 +0100

    Inserite dichiarazioni di funzioni di pt.c

---
+++ kern/include/pt.h
@@ -0,0 +1,25 @@
+#include <types.h>
+
+
+struct ipt_t{
+        pid_t pid;
+        vaddr_t vaddr;
+        bool invalid;
+        bool readonly;
+};
+
+
+int pt_init ( int num_ram_pages);
+
+void pt_add_entry( vaddr_t vaddr , paddr_t paddr, pid_t pid, bool readonly );
+
+int pt_get_paddr ( vaddr_t vaddr, pid_t pid , paddr_t* paddr);
+
+int pt_remove_entry (vaddr_t vaddr, pid_t pid);
+
+void pt_destroy ( void );
+
+struct ipt_t pt_get_entry (void); //funzione usata per verificare la presenza d>
+
+
+
