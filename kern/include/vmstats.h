baaf09aca8a6ea8d3868ee39857ebb47bccad475
llriccardo98ll <s248335@studenti.polito.it>
2021-12-20 11:41:04 +0100

    Definita la struttura dati per le statistiche della tlb

---
+++ kern/include/vmstats.h
@@ -0,0 +1,20 @@
+#ifndef _VM_STATS_H_
+#define _VM_STATS_H_
+
+#include <types.h>
+#include <kern/errno.h>
+
+
+typedef struct tlb_report{
+    unsigned int tlb_fault; // Numero totale di tlb fault
+    unsigned int tlb_faultFree; // Numero toale di tlb fault con celle libere della tlb
+    unsigned int tlb_faultReplacement; // Numero di tlb fault con rimpiazzamento
+    
+}tlb_report;
+
+extern struct tlb_report vmstats_report;
+
+// Inizializza i contatori della struttura dati
+struct tlb_report vmstats_init(void);
+
+#endif
--- kern/vm/vmstats.c
+++ kern/vm/vmstats.c
@@ -1,1 +1,13 @@
 // vmstats.c: code for tracking stats
+
+#include "vmstats.h"
+
+struct tlb_report vmstats_init(void){
+    struct tlb_report report;
+
+    report.tlb_fault = 0;
+    report.tlb_faultFree = 0;
+    report.tlb_faultReplacement = 0;
+
+    return report;
+}
